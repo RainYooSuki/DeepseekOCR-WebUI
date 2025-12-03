7ccc527c036f730da7eb10a3c1c1b225022be89c
RainYooSuki <1027030364@qq.com>
2025-12-03 12:51:02 +0800

    remove unused import

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 import module
 import argparse
-import sys
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='DeepSeek OCR')
     parser.add_argument('--input', type=str, default='./input', help='Input folder path')
     parser.add_argument('--output', type=str, default='./output', help='Output folder path')
     parser.add_argument('--model_path', type=str, default='./model', help='Model path')
-    
+
     args = parser.parse_args()
-    
+
     input_folder = args.input
     output_folder = args.output
     model_path = args.model_path
-    
+
     model = module.DocumentOCR(model_path=model_path)
     module.infer(input_folder, output_folder, model)
